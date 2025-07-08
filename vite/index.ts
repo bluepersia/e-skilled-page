7a1cd50958f8b6d483d58747ce451726c4772b29
Marco Zimmermann <fuzzyslash@hotmail.com>
2025-07-07 22:24:53 -0600

    Update

--- vite/index.ts
+++ vite/index.ts
@@ -5,3 +5,36 @@ window.addEventListener('load', () => {
 
     });
   });
+import {setInlineStyle, type InlineStyle} from 'fluid-scale';
+
+let activeInline:InlineStyle | null= null;
+
+function applyInlineTest ()
+{
+setTimeout (() => {
+  const title = document.querySelector ('[data-title]');
+  
+  if(title)
+  {
+   // title.classList.add (title.classList[0] + '--light');
+    //title.dataset.test = 'test';
+    activeInline = setInlineStyle (title, {
+      'font-size': '4.5rem',
+      'color': 'aqua'
+    })
+  }
+
+  setTimeout (() => {
+    if (title)
+    {
+      //title.classList.remove (title.classList[0] + '--light');
+      //title.dataset.test = '';
+     if(activeInline)
+        activeInline.undo ();
+
+     applyInlineTest ();
+    }
+  }, 2000)
+}, 2000)
+
+}
