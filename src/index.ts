9227bdb75dacff2efe535ce42d34b2774a6f9a98
Marco Zimmermann <fuzzyslash@hotmail.com>
2025-07-11 17:14:16 -0600

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,7 @@
 window.addEventListener('load', () => {
     import('fluid-scale').then((module) => {
-      module.default({json: 'homepage'}); // if the module exports an `init` function
+      module.default({scrollFix: true, json: 'homepage'}); // if the module exports an `init` function
 
 
     });
   });
-import {setInlineStyle, type InlineStyle} from 'fluid-scale';
-
-let activeInline:InlineStyle | null= null;
-
-function applyInlineTest ()
-{
-setTimeout (() => {
-  const title = document.querySelector ('[data-title]');
-  
-  if(title)
-  {
-   // title.classList.add (title.classList[0] + '--light');
-    //title.dataset.test = 'test';
-    activeInline = setInlineStyle (title, {
-      'font-size': '4.5rem',
-      'color': 'aqua'
-    })
-  }
-
-  setTimeout (() => {
-    if (title)
-    {
-      //title.classList.remove (title.classList[0] + '--light');
-      //title.dataset.test = '';
-     if(activeInline)
-        activeInline.undo ();
-
-     applyInlineTest ();
-    }
-  }, 2000)
-}, 2000)
-
-}
-
